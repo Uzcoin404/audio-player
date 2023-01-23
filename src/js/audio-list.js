7994717b09b006f1079367f053b66549ef1a9441
Your Name <uzcointg@gmail.com>
2023-01-23 21:41:08 +0500

    some changes

--- src/js/audio-list.js
+++ src/js/audio-list.js
@@ -3,24 +3,24 @@ let audioList = [
         name: "Security",
         artist: "Ecco2k",
         description: "Lorem ipsum dolor sit amet.",
-        src: "../../audio/_E.mp3",
+        src: "audio/_E.mp3",
     },
     {
         name: "FOUNDATION master",
         artist: "Unknown",
         description: "Lorem ipsum dolor sit amet.",
-        src: "../../audio/FOUNDATION-master.mp3",
+        src: "audio/FOUNDATION-master.mp3",
     },
     {
         name: "lightning",
         artist: "Unknown",
         description: "Lorem ipsum dolor sit amet.",
-        src: "../../audio/lightning.mp3",
+        src: "audio/lightning.mp3",
     },
     {
         name: "Oblivion master",
         artist: "Unknown",
         description: "Lorem ipsum dolor sit amet.",
-        src: "../../audio/Oblivion-master.mp3",
+        src: "audio/Oblivion-master.mp3",
     },
 ];
